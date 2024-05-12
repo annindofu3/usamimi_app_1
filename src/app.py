72a238b4045e4478e2a7f912a9256fc6de2d4872
松岡杏奈 <anna@MacBook-Pro.local>
2024-05-12 14:51:48 +0900

    Fix some issues

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def predict(img):
     # ネットワークの準備
     net = Net().cpu().eval()
     # # 学習済みモデルの重み（rabit_ears.pt）を読み込み
-    net.load_state_dict(torch.load('./src/rabit_ears.pt', map_location=torch.device('cpu')))
+    net.load_state_dict(torch.load('./rabit_ears.pt', map_location=torch.device('cpu')))
     #　データの前処理
     img = transform(img)
     img =img.unsqueeze(0) # 1次元増やす
